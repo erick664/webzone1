ed62b401e712e1baae8f2bb9021bcc55e2e9fd48
erick664 <47995687+erick664@users.noreply.github.com>
2020-08-25 23:45:53 -0700

    Update conexion.php

--- backend/models/conexion.php
+++ backend/models/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=cms","root","");
+		$link = new PDO("mysql:host=localhost;dbname=tzd6rlymeboc9kv0","g9o2a51aq8nbaczd","cy3gd7pdzu9mktqp");
 		return $link;
 
 	}
